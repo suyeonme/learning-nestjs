a5881a78fbfa7396361cf56dbbc30d8ca8152a91
Suyeon Kang <suyeon.dev@gmail.com>
2023-06-06 17:58:55 +0900

    feat(AuthService): Implement signin functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 
@@ -23,7 +27,7 @@ export class AuthService {
     const salt = randomBytes(8).toString('hex');
 
     // Hash the salt and the password together
-    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const hash = await this.hashPassword(password, salt);
 
     // Join the hashed result and the salt together (salt.hash)
     const result = salt + '.' + hash.toString('hex');
@@ -32,5 +36,22 @@ export class AuthService {
     return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = await this.hashPassword(password, salt);
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password');
+    }
+    return user;
+  }
+
+  async hashPassword(password: string, salt: string) {
+    return (await scrypt(password, salt, 32)) as Buffer;
+  }
 }
