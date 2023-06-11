cc5f764cbd374ca1078a3f3f7f615e09f400129f
Suyeon Kang <suyeon.dev@gmail.com>
2023-06-11 10:23:08 +0900

    test(auth.e2e-spec): Implement test for whoami

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -30,4 +30,21 @@ describe('Authentication System (e2e)', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('[whoami] signup as a new user then get the currently logged in user', async () => {
+    const email = 'abcdef@email.com';
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'abcde' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
