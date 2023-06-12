768ccadc6142e7f78257280ab276159a82389a56
Suyeon Kang <suyeon.dev@gmail.com>
2023-06-12 21:22:31 +0900

    refactor(ormconfig): Create ormconfig.js and implement it

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+var dbConfig = {
+  synchronize: false,
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'], // *
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'], // *
+    });
+    break;
+  case 'production':
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
