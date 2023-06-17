72f9323d74d07216522db6bcb337aae8503fbd28
Suyeon Kang <suyeon.dev@gmail.com>
2023-06-17 14:09:39 +0900

    chore(Procfile): connect to Heroku

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,19 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
- 
+
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
- 
+
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-    return {
-      type: 'sqlite',
-      database: this.configService.get<string>('DB_NAME'),
-      autoLoadEntities: true,
-      synchronize: process.env.NODE_ENV === 'test',
-      migrationsRun: process.env.NODE_ENV === 'test',
-      keepConnectionAlive: process.env.NODE_ENV === 'test',
-    };
+    switch (process.env.NODE_ENV) {
+      case 'production':
+        return {
+          type: 'postgres',
+          url: process.env.DATABASE_URL,
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: false,
+          migrationsRun: true,
+          keepConnectionAlive: false,
+          ssl: { rejectUnauthorized: false },
+        };
+      case 'test':
+        return {
+          type: 'sqlite',
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          migrationsRun: true,
+          keepConnectionAlive: true,
+        };
+      default:
+        return {
+          type: 'sqlite',
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          migrationsRun: false,
+          keepConnectionAlive: true,
+        };
+    }
   }
 }
